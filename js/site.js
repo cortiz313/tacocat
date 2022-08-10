9eaa222f5267bf66679f3871a7ca540207308328
Christian Ortiz <c.ortiz313@gmail.com>
2022-08-10 11:08:13 -0400

    Basic setup
    All the code reflects the taco cat project but its not complete

--- js/site.js
+++ js/site.js
@@ -1,4 +1,21 @@
-// Call Hello World
-function helloWorld() {
-alert("hello World");
+// Get user input
+function getValue(){
+    //get user string from page
+    let userString = document.getElementById("userString").value;
+
+    // check for a palindrome
+    let returnObj = checkForPalindrome(userString);
+
+    // display our message to the screen
+    displayMessage(returnObj);
+}
+
+// check if string is a palindrome
+function checkForPalindrome(){
+
+}
+
+// display a message to the user
+function displayMessage(){
+
 }
